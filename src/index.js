029a563049242d00d1e282a5e4c82f21b96c2bc2
NicolasBouhours <nico53960@hotmail.fr>
2017-04-01 14:25:55 +0200

    React router and basic redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import { BrowserRouter as Router, Route, IndexRoute, browserHistory } from 'react-router-dom'
+import reduxThunk from 'redux-thunk'
+
 import App from './App';
-import './index.css';
+import Home from './components/home'
+import Welcome from './components/welcome'
+import reducers from './reducers'
+
+const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore)
+const store = createStoreWithMiddleware(reducers)
+
+import './App.css';
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  <Provider store={store}>
+    <Router>
+      <div>
+        <Route exact path="/" component={Home} />
+        <Route path="/welcome" component={Welcome} />
+      </div>
+    </Router>
+  </Provider>
+  , document.getElementById('root')
 );
